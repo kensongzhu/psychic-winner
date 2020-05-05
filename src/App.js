0e38db639b7a722e1a975f5789fabf4687b9ecec
Wanji <wanji.zhu@eifer.uni-karlsruhe.de>
2020-05-05 16:13:19 +0200

    setup fonts (proxixma nova regular, semi-bold and bold)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Footer from "./components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-import Main from "./components/Main";
 
 function App() {
   return (
     <Router>
       <div className={"container-fluid d-flex flex-column wrapper p-0"}>
-        <Main />
-        <Footer />
+        {/*<Main />*/}
+        {/*<Footer />*/}
+        <h1>Sign in</h1>
       </div>
     </Router>
   );
