2a1eb3fbef3e05419b75a83371abc4df6fde317a
Wanji <wanji.zhu@eifer.uni-karlsruhe.de>
2020-05-08 13:24:10 +0200

    Updates

---
+++ precache-manifest.35d7704e4c03d94ab809a4c73672b15a.js
@@ -0,0 +1,95 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "7c6f07e1594fc4b336ce31b16b53077f",
+    url: "/calobs-home/index.html",
+  },
+  {
+    revision: "abb66607438c79c70c5b",
+    url: "/calobs-home/static/css/main.dc0c8d7e.chunk.css",
+  },
+  {
+    revision: "63e8360908377ce3c796",
+    url: "/calobs-home/static/js/2.88b0778a.chunk.js",
+  },
+  {
+    revision: "c64c486544348f10a6d6c716950bc223",
+    url: "/calobs-home/static/js/2.88b0778a.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "abb66607438c79c70c5b",
+    url: "/calobs-home/static/js/main.8d63d998.chunk.js",
+  },
+  {
+    revision: "b2f78f4700f76e49afca",
+    url: "/calobs-home/static/js/runtime-main.cfe2951f.js",
+  },
+  {
+    revision: "eadb60b941cd3ebcd2c6e3254a16ab5b",
+    url: "/calobs-home/static/media/calobs.eadb60b9.svg",
+  },
+  {
+    revision: "3328cd254f7e093657842204b7e18fdc",
+    url: "/calobs-home/static/media/captcha.3328cd25.png",
+  },
+  {
+    revision: "eff17a7c51c24a7a6e90824db9e5a261",
+    url: "/calobs-home/static/media/edf.eff17a7c.svg",
+  },
+  {
+    revision: "262a97efe8654635841a93646bc053ee",
+    url: "/calobs-home/static/media/eifer.262a97ef.svg",
+  },
+  {
+    revision: "18356dda7f475271c89b628c7ef1a070",
+    url: "/calobs-home/static/media/illustration.18356dda.svg",
+  },
+  {
+    revision: "1a7072315573447fc86c131624af30f1",
+    url: "/calobs-home/static/media/proxima-nova-bold-webfont.1a707231.svg",
+  },
+  {
+    revision: "270430420b0733e6df842c721135557b",
+    url: "/calobs-home/static/media/proxima-nova-bold-webfont.27043042.eot",
+  },
+  {
+    revision: "36cd36ef10dcc583c7199324e81030a1",
+    url: "/calobs-home/static/media/proxima-nova-bold-webfont.36cd36ef.ttf",
+  },
+  {
+    revision: "c6820c31759aff2f730eb0f64fff4cdd",
+    url: "/calobs-home/static/media/proxima-nova-bold-webfont.c6820c31.woff",
+  },
+  {
+    revision: "3d356e041336b873d26fb3c4318a7a5d",
+    url: "/calobs-home/static/media/proxima-nova-regular-webfont.3d356e04.woff",
+  },
+  {
+    revision: "6de755eb3f52c7ad09dc7ff52ce1cd34",
+    url: "/calobs-home/static/media/proxima-nova-regular-webfont.6de755eb.svg",
+  },
+  {
+    revision: "c4b5bff885586e3123a70889042de719",
+    url: "/calobs-home/static/media/proxima-nova-regular-webfont.c4b5bff8.ttf",
+  },
+  {
+    revision: "dc7e7e4aa0005e46d11e69943d7e1070",
+    url: "/calobs-home/static/media/proxima-nova-regular-webfont.dc7e7e4a.eot",
+  },
+  {
+    revision: "209e776ca2604c737e5149ca0a710622",
+    url: "/calobs-home/static/media/proxima-nova-semibold-webfont.209e776c.eot",
+  },
+  {
+    revision: "50fccecae790b01a94a613a1fca8d9af",
+    url: "/calobs-home/static/media/proxima-nova-semibold-webfont.50fcceca.ttf",
+  },
+  {
+    revision: "68677230389fada78c5236fe69017e91",
+    url:
+      "/calobs-home/static/media/proxima-nova-semibold-webfont.68677230.woff",
+  },
+  {
+    revision: "78604042f7bb8ee3364d6cb0441d44bb",
+    url: "/calobs-home/static/media/proxima-nova-semibold-webfont.78604042.svg",
+  },
+]);
