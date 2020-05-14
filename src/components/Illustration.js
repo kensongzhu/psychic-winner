24968bf9c4c17face66ac322351600ec288bebf1
Wanji <wanji.zhu@eifer.uni-karlsruhe.de>
2020-05-14 13:40:25 +0200

    remove transition

--- src/components/Illustration.js
+++ src/components/Illustration.js
@@ -1,6 +1,5 @@
 import illustration from "../illustration.svg";
 import React from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const Illustration = () => (
   <div className={"col-12 col-lg-6 row justify-content-center"}>
@@ -9,17 +8,7 @@ const Illustration = () => (
       alt={"illustration"}
       className={"col-9 col-sm-8 col-lg-12 col-xl-9 d-none"}
     />
-    <TransitionGroup className={"w-100"}>
-      <CSSTransition
-        classNames="moveFromBottomFade"
-        timeout={700}
-        appear={true}
-        exit={false}
-        unmountOnExit={true}
-      >
-        <div className={"hero"} />
-      </CSSTransition>
-    </TransitionGroup>
+    <div className={"hero"} />
   </div>
 );
 
--- src/components/MainView.js
+++ src/components/MainView.js
@@ -3,7 +3,6 @@ import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
 import ForgotPassword from "./ForgotPassForm";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const MainView = () => {
   const location = useLocation();
@@ -11,22 +10,11 @@ const MainView = () => {
   return (
     <div className={"col-12 col-lg-6 row no-gutters"}>
       <div className={"col-12 col-lg-10 col-xl-8"}>
-        <TransitionGroup>
-          <CSSTransition
-            classNames="moveFromRightFade"
-            timeout={700}
-            appear={true}
-            exit={false}
-            key={location.key}
-            unmountOnExit={true}
-          >
-            <Switch location={location}>
-              <Route exact path={"/"} component={LoginForm} />
-              <Route path={"/join-now"} component={SignUpForm} />
-              <Route path={"/forgot-password"} component={ForgotPassword} />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
+        <Switch location={location}>
+          <Route exact path={"/"} component={LoginForm} />
+          <Route path={"/join-now"} component={SignUpForm} />
+          <Route path={"/forgot-password"} component={ForgotPassword} />
+        </Switch>
       </div>
     </div>
   );
